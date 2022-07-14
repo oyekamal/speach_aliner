084a83e165134fd829d5fb049f7be003d782f2ed
jalal <autocratic.jalal@gmail.com>
2022-07-14 20:50:30 +0500

    update kamal

--- clean/practice_code/changing_color_of_object.py
+++ clean/practice_code/changing_color_of_object.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def changing_color_using_hsv(img, color = [34,255,255], lower_hsv_value=[0,  0, 78], upper_hsv_value=[255, 255, 208]):
+def changing_color_using_hsv(img, color = [34,0,100], lower_hsv_value=[0,  0, 78], upper_hsv_value=[255, 255, 208]):
     # extract alpha channel
     alpha = img[:,:,3]
 
@@ -24,7 +24,7 @@ def changing_color_using_hsv(img, color = [34,255,255], lower_hsv_value=[0,  0, 
 
 
     return bgra
-img = cv2.imread('eyes.png', cv2.IMREAD_UNCHANGED)
+img = cv2.imread('eyes2.png', cv2.IMREAD_UNCHANGED)
 color_change_image=changing_color_using_hsv(img)
 cv2.imwrite('sword_masked_green.png', color_change_image)
 
